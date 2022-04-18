b41e61351b395666a6e677e0ed8bd2a24a05d9ca
zwq <1512908947@qq.com>
2022-04-18 14:43:26 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home_wrap">
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    export default {
+        name: 'Home',
+        components: {},
+        data() {
+            return {}
+        },
+        methods: {},
+        mounted() {
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+        }
+    }
 </script>
+
+<style lang="less">
+    .home_wrap {
+        width: 100%;
+        height: 100%;
+    }
+</style>
